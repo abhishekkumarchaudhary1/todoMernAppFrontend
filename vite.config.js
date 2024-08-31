d8c13d11e1f00372605da212acbe86a83f44e2c3
abhishekkumarchaudhary1 <abhishek.chaudhary33@gmail.com>
2024-08-31 21:17:49 +0530

    trying to re-establish full stack connection

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const BASE_URL = 'https://todomernappbackend-8fju.onrender.com'
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/myApi': {
-        target: `${BASE_URL}/api/v1`,
+        target: `https://todomernappbackend-8fju.onrender.com/api/v1`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/myApi/, ''),
       },
